e6cdbc0764332f8fc0c9afa6bab6b24d0c2fbea5
bongwe <obomvu@gmail.com>
2019-05-13 01:33:54 +0200

    added sql

---
+++ sql/Tables/AcoountTable.sql
@@ -0,0 +1,19 @@
+BEGIN TRAN
+
+CREATE TABLE Account(
+	id INT NOT NULL IDENTITY(1,1),
+	name VARCHAR(128) NOT NULL,
+	username VARCHAR(128) NOT NULL,
+	email VARCHAR(128) NOT NULL,
+	password VARCHAR(128) NOT NULL,
+	interests VARCHAR(500) NOT NULL,
+	PRIMARY KEY(email)
+);
+
+ALTER TABLE Account
+ALTER COLUMN interests VARCHAR(500);
+
+select * from Account;
+
+--ROLLBACK
+--COMMIT
