fb9f6498bd001498397e13ee01f68dd354a0dfff
bongwe <obomvu@gmail.com>
2019-06-27 01:26:29 +0200

    updated a script

--- sql/DeleteAll.sql
+++ sql/DeleteAll.sql
@@ -1,5 +1,16 @@
-delete from Account;
-delete from BOOKSECTION;
-delete from BOOKSHELF;
-delete from SETTINGS;
-delete from Account;
+select * from book_section;
+select * from selected_opponent;
+select * from Account;
+select * from settings;
+select * from game_state;
+select * from selected_opponent;
+select * from player;
+select * from book_shelf;
+
+delete from book_shelf;
+delete from settings;
+delete from account;
+delete from book_section;
+delete from selected_opponent;
+delete from player;
+delete from game_state;
